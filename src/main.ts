4b0d8b98548e7d80a5e54127256811c1e6376017
Spookyless <spookyless.info@gmail.com>
2023-11-03 17:12:28 +0100

    Create the main server

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,52 @@
+import path from 'path';
 import express from 'express';
+import { WebSocketServer } from 'ws';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { createServer } from 'http';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@apollo/server/express4';
+import { BootstrapSchema, BootstrapResolvers  } from './graphql-bootstrap';
+import { ServerCleanupPlugin, LogResolvedFieldsPlugin } from "./apollo-plugins";
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const PORT = parseInt(process?.env?.PORT || '4000', 10);
 
-const app = express();
+(async () => {
+  // Bootstrap schema and resolvers
+  const typeDefs = await BootstrapSchema(path.join(__dirname, './graphql'));
+  const resolvers = await BootstrapResolvers(path.join(__dirname, './graphql'));
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
+  // Create the schema
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+  // Create all the necessary servers
+  const app = express();
+  const httpServer = createServer(app);
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/graphql',
+  });
+
+  const serverCleanup = useServer({ schema }, wsServer);
+
+  const apolloServer = new ApolloServer({
+    resolvers,
+    typeDefs,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ServerCleanupPlugin(serverCleanup),
+      LogResolvedFieldsPlugin()
+    ],
+  });
+
+  // Start Apollo before starting the main server
+  await apolloServer.start();
+
+  app.use('/graphql', express.json(), expressMiddleware(apolloServer));
+
+  // Start the main server
+  await new Promise<void>((resolve) => httpServer.listen(PORT, resolve));
+  console.log("GraphQL server is running");
+})();
