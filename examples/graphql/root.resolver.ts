55cdf75af874398c453fab953179e8b70e3dddd3
Spookyless <spookyless.info@gmail.com>
2023-11-03 17:52:58 +0100

    Added GQL usage example

---
+++ examples/graphql/root.resolver.ts
@@ -0,0 +1,15 @@
+const resolver = {
+  Query: {
+    cards() {
+      return {};
+    },
+  },
+  Mutation: {
+    cards() {
+      return {};
+    }
+  },
+  Subscription: {}
+};
+
+export {resolver}
