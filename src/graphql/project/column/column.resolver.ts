c0af92fc67bfacb4f32eb78a0aa218ab58472f36
Spookyless <spookyless.info@gmail.com>
2023-11-04 13:28:10 +0100

    Added cards

---
+++ src/graphql/project/column/column.resolver.ts
@@ -0,0 +1,16 @@
+import { SQL } from '../../../database/Connection';
+
+const resolver = {
+  Column: {
+    cards(parent) {
+      const result = SQL`
+          SELECT card_id as id, name, description, story_points, creation_timestamp as created, "order"
+          FROM core.cards
+          WHERE column_id = ${parent.id}`;
+
+      return result;
+    },
+  },
+};
+
+export { resolver };
