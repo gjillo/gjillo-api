ee42bd4360d7a21f0363604d1c8d9df17bccfabb
Spookyless <spookyless.info@gmail.com>
2023-11-11 17:34:07 +0100

    Added schemas and resolvers for additional data

---
+++ src/graphql/project/column/card/card.resolver.ts
@@ -0,0 +1,39 @@
+import { SQL } from '../../../../database/Connection';
+
+const resolver = {
+  Card: {
+    async assignee(parent) {
+      const result = await SQL`
+          SELECT auth.users.id as uuid, auth.users.name as name, email, image
+          FROM core.cards
+              JOIN core.card_users USING (card_uuid)
+              JOIN auth.users ON core.card_users.user_uuid = auth.users.id
+          WHERE card_uuid = ${parent.uuid}`;
+
+      return result[0];
+    },
+    async deadline(parent) {
+      const result = await SQL`
+          SELECT value
+          FROM core.cards
+              JOIN core.date_values USING (card_uuid)
+              JOIN core.fields USING (field_uuid)
+          WHERE card_uuid = ${parent.uuid}
+            AND role = 'deadline'`;
+
+      return result[0]['value'];
+    },
+    tags(parent) {
+      return SQL`
+          SELECT value, color
+          FROM core.cards
+              JOIN core.select_values USING (card_uuid)
+              JOIN core.select_options USING (select_option_uuid)
+              JOIN core.fields ON core.fields.field_uuid = select_values.field_uuid
+          WHERE card_uuid = ${parent.uuid}
+            AND role = 'tags'`;
+    },
+  },
+};
+
+export { resolver };
