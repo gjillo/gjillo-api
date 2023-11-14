39db8070ebf2bfb4473e63795ddcc79ee78b7cdd
Spookyless <spookyless.info@gmail.com>
2023-11-14 16:12:38 +0100

    Add comments for GQL schema

--- src/graphql/cardDetails/cardDetails.resolver.ts
+++ src/graphql/cardDetails/cardDetails.resolver.ts
@@ -12,15 +12,13 @@ const resolver = {
     },
   },
   CardDetails: {
-    async assignee(parent) {
-      const result = await SQL`
+    assignees(parent) {
+      return SQL`
           SELECT auth.users.id as uuid, auth.users.name as name, email, image
           FROM core.cards
               JOIN core.card_users USING (card_uuid)
               JOIN auth.users ON core.card_users.user_uuid = auth.users.id
           WHERE card_uuid = ${parent.uuid}`;
-
-      return result[0];
     },
     async deadline(parent) {
       const result = await SQL`
