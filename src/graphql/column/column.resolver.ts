0d8f097f899bcd8d206339c5011bb1797f69fd64
Krzysztof Wrona <rubikon02@gmail.com>
2023-11-15 13:19:16 +0100

    Add column subscriptions

--- src/graphql/column/column.resolver.ts
+++ src/graphql/column/column.resolver.ts
@@ -1,4 +1,5 @@
 import { SQL } from '../../database/Connection';
+import {pubsub} from "../context";
 
 const resolver = {
   Column: {
@@ -12,4 +13,39 @@ const resolver = {
   },
 };
 
-export { resolver };
+const mutation = {
+  ColumnMutation: {
+    async create(_, { name }) {
+      void pubsub.publish("column/created", {name});
+    },
+
+    async update(_, { uuid, name }) {
+      void pubsub.publish("column/updated", {uuid, name});
+    },
+
+    async add_card(_, { uuid }) {
+      void pubsub.publish("column/card_added", {uuid});
+    },
+
+    async remove_card(_, { uuid }) {
+      void pubsub.publish("column/card_removed", {uuid});
+    },
+  },
+};
+
+const subscription = {
+  column_created: {
+    subscribe: () => pubsub.asyncIterator(['column/created'])
+  },
+  column_updated: {
+    subscribe: () => pubsub.asyncIterator(['column/updated'])
+  },
+  column_card_added: {
+    subscribe: () => pubsub.asyncIterator(['column/card_added'])
+  },
+  column_card_removed: {
+    subscribe: () => pubsub.asyncIterator(['column/card_removed'])
+  },
+}
+
+export {resolver, mutation, subscription}
